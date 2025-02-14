99ac39c6db2287d56eb67670f40c298b4f08fc9c
irvin samboy <contacoirvinsamboy@gmail.com>
2025-02-14 00:00:07 -0400

    initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="flex items-center justitfy-center flex-col">
+        <h2 className="text-[#C82528] text-center text-5xl font-black">Do you wanna be my valentine?</h2>
+        <div>
+          <button>Yes!</button>
+          <button>No</button>
+        </div>
     </div>
   )
 }
